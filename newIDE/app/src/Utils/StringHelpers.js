404c9679dee731e5d9b8359b6c50cb82c0cf5310
Sebastian Sangervasi <villain@harmless.dev>
2021-02-13 14:32:10 -0800

    Add helper for safely parsing floats
    This commit introduces a string parsing funtion that will return a
    number that is is never NaN or Infinity.

--- newIDE/app/src/Utils/StringHelpers.js
+++ newIDE/app/src/Utils/StringHelpers.js
@@ -1,5 +1,19 @@
 // @flow
+import clamp from 'lodash/clamp';
 
 export const shortenString = (str: string, maxLength: number) => {
   return str.length > maxLength ? str.substring(0, maxLength) + '...' : str;
 };
+
+/**
+ * parseFloatSafe parses a string to a number that is never Nan or Infinity.
+ * If the numer would be NaN, it is returned as zero.
+ * Negative/positive Infinity is clamped to the min/max safe JS integer.
+ */
+export const parseFloatSafe = (str?: string): number => {
+  const parsed = Number.parseFloat(str)
+  if (Number.isNaN(parsed)) {
+    return 0
+  }
+  return clamp(parsed, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+}
